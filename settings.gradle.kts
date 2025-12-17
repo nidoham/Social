3f3d75f10bc380fd44cd480c6b501ab6c1df1cca
nidoham <nidohamondol.official@gmail.com>
2025-12-18 02:50:09 +0600

    Initial

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,11 +11,26 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+
+        // GitHub Packages for publishing
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/nidoham/Social")
+            credentials {
+                username = providers.gradleProperty("gpr.user")
+                    .orElse(providers.environmentVariable("GITHUB_ACTOR"))
+                    .orNull
+                password = providers.gradleProperty("gpr.token")
+                    .orElse(providers.environmentVariable("GITHUB_TOKEN"))
+                    .orNull
+            }
+        }
     }
 }
 
--- social/build.gradle.kts
+++ social/build.gradle.kts
@@ -1,17 +1,15 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
 }
 
 android {
     namespace = "com.nidoham.social"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -25,12 +23,21 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
     }
 }
 
@@ -44,4 +51,57 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.nidoham"
+                artifactId = "social"
+                version = "1.0.0"
+
+                pom {
+                    name.set("Social Library")
+                    description.set("A social networking library for Android with Firebase integration")
+                    url.set("https://github.com/nidoham/Social")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("nidoham")
+                            name.set("Nidoham")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/nidoham/Social.git")
+                        developerConnection.set("scm:git:ssh://github.com/nidoham/Social.git")
+                        url.set("https://github.com/nidoham/Social")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/nidoham/Social")
+                credentials {
+                    username = "nidoham"
+                    password = findProperty("gpr.token") as String?
+                        ?: System.getenv("GITHUB_TOKEN")
+                                ?: throw GradleException("GitHub token not found. Set GITHUB_TOKEN environment variable or gpr.token in gradle.properties")
+                }
+            }
+        }
+    }
 }
